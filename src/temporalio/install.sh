440f7c0a200181f1267e3d869874835ff9ca1105
Favalos <favalos.eth@ethermail.io>
2023-04-22 14:48:39 -0700

    Add Nano Layer.

--- src/temporalio/install.sh
+++ src/temporalio/install.sh
@@ -4,31 +4,19 @@ set -e
 
 source ./library_scripts.sh
 
-echo "Activating feature 'temporalio'"
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.4.28"
 
-TMP_FOLDER="$_CONTAINER_USER_HOME/tmp"
-TEMPORALITE_VERSION="0.3.0"
-NAMESPACE=["$NAMESPACE"="default"]
-TEMPORALITE_URL="https://github.com/temporalio/temporalite/releases/download/v${TEMPORALITE_VERSION}/temporalite_${TEMPORALITE_VERSION}_linux_amd64.tar.gz"
-TEMPORALITE_FILE="temporalite_linux_amd64.tar.gz"
-TEMPORLITE_EXEC="temporalite"
 
-echo "Creating a temp folder for Temporalio: '$TMP_FOLDER'"
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.14" \
+    --option repo='temporalio/temporalite' --option binaryNames='temporalite' --option version="v0.3.0"
 
-mkdir $TMP_FOLDER
-cd $TMP_FOLDER
 
-echo "Download Temporalite release from: ${TEMPORALITE_URL}"
-clean_download $TEMPORALITE_URL $TEMPORALITE_FILE 
-
-tar -xf $TEMPORALITE_FILE
-
-echo "Move Temporalite to Local bin"
-mv $TEMPORLITE_EXEC /usr/local/bin
-
-echo "Clean temp folder for Temporalio"
-rm -rf $TMP_FOLDER
-
-chmod +x /usr/local/bin/$TEMPORLITE_EXEC
-
-/usr/local/bin/${TEMPORLITE_EXEC} start --n $NAMESPACE --ephemeral &
+echo 'Done!'
--- src/temporalio/library_scripts.sh
+++ src/temporalio/library_scripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -i
 
+
 clean_download() {
     # The purpose of this function is to download a file with minimal impact on contaier layer size
     # this means if no valid downloader is found (curl or wget) then we install a downloader (currently wget) in a 
@@ -21,7 +22,7 @@ clean_download() {
         # copy current state of apt list - in order to revert back later (minimize contianer layer size) 
         cp -p -R /var/lib/apt/lists $tempdir 
         apt-get update -y
-        apt-get -y install --no-install-recommends wget
+        apt-get -y install --no-install-recommends wget ca-certificates
     }
 
     function _apt_get_cleanup() {
@@ -32,7 +33,7 @@ clean_download() {
 
         echo "revert back apt lists"
         rm -rf /var/lib/apt/lists/*
-        mv -v $tempdir/lists/* /var/lib/apt/lists  
+        rm -r /var/lib/apt/lists && mv $tempdir/lists /var/lib/apt/lists
     }
 
     function _apk_install() {
@@ -90,5 +91,87 @@ clean_download() {
             exit 1
         fi
     fi 
+
+}
+
+
+ensure_nanolayer() {
+    # Ensure existance of the nanolayer cli program
+    local variable_name=$1
+
+    local required_version=$2
+    # normalize version
+    if ! [[ $required_version == v* ]]; then
+        required_version=v$required_version
+    fi
+
+    local nanolayer_location=""
+
+    # If possible - try to use an already installed nanolayer
+    if [[ -z "${NANOLAYER_FORCE_CLI_INSTALLATION}" ]]; then
+        if [[ -z "${NANOLAYER_CLI_LOCATION}" ]]; then
+            if type nanolayer >/dev/null 2>&1; then
+                echo "Found a pre-existing nanolayer in PATH"
+                nanolayer_location=nanolayer
+            fi
+        elif [ -f "${NANOLAYER_CLI_LOCATION}" ] && [ -x "${NANOLAYER_CLI_LOCATION}" ] ; then
+            nanolayer_location=${NANOLAYER_CLI_LOCATION}
+            echo "Found a pre-existing nanolayer which were given in env varialbe: $nanolayer_location"
+        fi
+
+        # make sure its of the required version
+        if ! [[ -z "${nanolayer_location}" ]]; then
+            local current_version
+            current_version=$($nanolayer_location --version)
+            if ! [[ $current_version == v* ]]; then
+                current_version=v$current_version
+            fi
+
+            if ! [ $current_version == $required_version ]; then
+                echo "skipping usage of pre-existing nanolayer. (required version $required_version does not match existing version $current_version)"
+                nanolayer_location=""
+            fi
+        fi
+
+    fi
+
+    # If not previuse installation found, download it temporarly and delete at the end of the script 
+    if [[ -z "${nanolayer_location}" ]]; then
+
+        if [ "$(uname -sm)" == "Linux x86_64" ] || [ "$(uname -sm)" == "Linux aarch64" ]; then
+            tmp_dir=$(mktemp -d -t nanolayer-XXXXXXXXXX)
+
+            clean_up () {
+                ARG=$?
+                rm -rf $tmp_dir
+                exit $ARG
+            }
+            trap clean_up EXIT
+
+            
+            if [ -x "/sbin/apk" ] ; then
+                clib_type=musl
+            else
+                clib_type=gnu
+            fi
+
+            tar_filename=nanolayer-"$(uname -m)"-unknown-linux-$clib_type.tgz
+
+            # clean download will minimize leftover in case a downloaderlike wget or curl need to be installed
+            clean_download https://github.com/devcontainers-contrib/cli/releases/download/$required_version/$tar_filename $tmp_dir/$tar_filename
+            
+            tar xfzv $tmp_dir/$tar_filename -C "$tmp_dir"
+            chmod a+x $tmp_dir/nanolayer
+            nanolayer_location=$tmp_dir/nanolayer
+      
+
+        else
+            echo "No binaries compiled for non-x86-linux architectures yet: $(uname -m)"
+            exit 1
+        fi
+    fi
+
+    # Expose outside the resolved location
+    declare -g ${variable_name}=$nanolayer_location
 
 }
